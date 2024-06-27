4372b928405e95047e6b7f8d319f640904fe8fa5
Pritam <pritamgujar27@gmail.com>
2024-06-27 10:07:04 +0530

    Added APIs of ToDo component

--- controller/task.js
+++ controller/task.js
@@ -31,6 +31,67 @@ const addTask = async (req, res) => {
   }
 };
 
+const getTask = async (req, res, next) => {
+  try {
+    const category = req.query.category;
+
+    let taskDetails = await Task.find({ queue: category });
+    console.log(taskDetails);
+
+    taskDetails = Array.from(taskDetails);
+
+    return res.json({
+      data: taskDetails,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateQueueOnTask = async (req, res, next) => {
+  try {
+    const taskId = req.query.id || "";
+    const updatedQueue = req.query.queue || "";
+
+    if (!taskId) {
+      return res.status(400).json({
+        message: "Bad Request",
+      });
+    }
+
+    const taskDetails = await Task.findOne({
+      _id: taskId,
+    });
+
+    if (!taskDetails) {
+      return res.status(400).json({
+        message: "Bad request",
+      });
+    }
+
+    await Task.updateOne(
+      { _id: taskId },
+      {
+        $set: {
+          title: taskDetails?.title,
+          priority: taskDetails?.priority,
+          assignedTo: taskDetails?.assignedTo,
+          queue: updatedQueue,
+          tasks: taskDetails?.tasks,
+          dueDate: taskDetails?.dueDate,
+          user: taskDetails?.user,
+        },
+      }
+    );
+
+    res.json({ message: "Task updated successfully", updated: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   addTask,
+  getTask,
+  updateQueueOnTask,
 };
--- routes/task.js
+++ routes/task.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const taskController = require("../controller/task");
 
 router.post("/add", taskController.addTask);
+router.get("/getTask", taskController.getTask);
+router.put("/updateQueue", taskController.updateQueueOnTask);
 
 module.exports = router;
